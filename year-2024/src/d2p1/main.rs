86b74e9e04606a81bd50173378ef3ee76bd5ee0e
acehinnnqru <acehinnnqru@gmail.com>
2024-12-15 22:07:50 +0800

    chore: make day2 part1 prettier

--- year-2024/src/d2p1/main.rs
+++ year-2024/src/d2p1/main.rs
@@ -13,38 +13,31 @@ fn main() {
     // build heaps
     let count = lines
         .map_while(Result::ok)
-        .map(|s| -> (String, bool) {
-            let mut ns = s.split_whitespace();
-            let first = ns.next().unwrap().parse::<i32>().unwrap();
-            let second = ns.next().unwrap().parse::<i32>().unwrap();
+        .filter(|s| -> bool {
+            let mut ns = s.split_whitespace().map(|s| s.parse::<i32>().unwrap());
+            let first = ns.next().unwrap();
+            let second = ns.next().unwrap();
 
             let diff = second - first;
             if diff == 0 || diff.abs() > 3 {
-                return (s, false);
+                return false;
             }
 
             let flag = diff > 0;
             let mut pre = second;
 
             for v in ns {
-                let cur = v.parse::<i32>().unwrap();
+                let cur = v;
                 let diff = cur - pre;
                 if diff == 0 || diff.abs() > 3 || (diff > 0) != flag {
-                    return (s, false);
+                    return false;
                 }
 
                 pre = cur
             }
 
-            (s, true)
+            true
         })
-        .map(|(s, v)| -> bool {
-            if !v {
-                println!("{}", s);
-            }
-            v
-        })
-        .filter(|v| *v)
         .count();
 
     println!("{}", count);
