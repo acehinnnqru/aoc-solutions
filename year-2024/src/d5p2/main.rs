1eeedcfc359c446d12230d54d02768aa3eddccd6
acehinnnqru <acehinnnqru@gmail.com>
2024-12-16 23:53:08 +0800

    feat: day5 part2 done

---
+++ year-2024/src/d5p2/main.rs
@@ -0,0 +1,75 @@
+use std::{
+    cmp::Ordering,
+    collections::HashSet,
+    env,
+    fs::File,
+    io::{BufRead, BufReader, Lines},
+};
+
+// a < b
+type Less = (i32, i32);
+type Rule = HashSet<Less>;
+
+fn less(rule: &Rule, a: i32, b: i32) -> bool {
+    rule.contains(&(a, b))
+}
+
+fn main() {
+    // read data file
+    let path = env::args().nth(1).unwrap();
+    let file = File::open(path).unwrap();
+    let mut lines = BufReader::new(file).lines();
+
+    let rule = build_rules(&mut lines);
+
+    // println!("{:?}", afters);
+
+    let ret = lines
+        .map_while(Result::ok)
+        .filter_map(|s| -> Option<Vec<i32>> {
+            let nums = s
+                .split(",")
+                .map(|v| v.parse().unwrap())
+                .collect::<Vec<i32>>();
+
+            for i in 1..nums.len() {
+                if !less(&rule, nums[i - 1], nums[i]) {
+                    return Some(nums);
+                }
+            }
+
+            None
+        })
+        .map(|mut nums| -> i32 {
+            // println!("{:?}", nums);
+
+            nums.sort_by(|a, b| {
+                if less(&rule, *a, *b) {
+                    Ordering::Less
+                } else {
+                    Ordering::Greater
+                }
+            });
+
+            nums[nums.len() / 2]
+        })
+        .sum::<i32>();
+
+    println!("{}", ret);
+}
+
+fn build_rules(lines: &mut Lines<BufReader<File>>) -> Rule {
+    let mut rules = Rule::new();
+    loop {
+        let line = lines.next().unwrap().unwrap();
+        if line.trim().is_empty() {
+            break;
+        }
+
+        let (former, latter) = line.split_once("|").unwrap();
+
+        rules.insert((former.parse().unwrap(), latter.parse().unwrap()));
+    }
+
+    rules
+}
