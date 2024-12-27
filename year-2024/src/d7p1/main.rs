16e9d3a18de09a367f85a6fcdc7b839a7c3f056c
acehinnnqru <acehinnnqru@gmail.com>
2024-12-27 19:47:42 +0800

    feat: day7 part1 done

---
+++ year-2024/src/d7p1/main.rs
@@ -0,0 +1,78 @@
+use std::{
+    env,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() {
+    // read data file
+    let path = env::args().nth(1).unwrap();
+    let file = File::open(path).unwrap();
+    let lines = BufReader::new(file).lines();
+
+    let ret = lines
+        .map_while(Result::ok)
+        .map(extract)
+        .map(|(target, nums)| check(target, nums))
+        .filter(|v| v.is_some())
+        .sum::<Option<i64>>()
+        .unwrap();
+
+    println!("{ret}");
+}
+
+fn extract(s: String) -> (i64, Vec<i64>) {
+    let (first, second) = s.split_once(":").unwrap();
+
+    let target = first.parse::<i64>().unwrap();
+    let nums = second
+        .split_whitespace()
+        .map(|v| v.parse::<i64>().unwrap())
+        .collect::<Vec<i64>>();
+
+    (target, nums)
+}
+
+fn check(target: i64, nums: Vec<i64>) -> Option<i64> {
+    if backtrace(0, Op::Plus, &nums, &target) {
+        return Some(target);
+    }
+
+    if backtrace(1, Op::Multi, &nums, &target) {
+        return Some(target);
+    }
+
+    None
+}
+
+enum Op {
+    Plus,
+    Multi,
+}
+
+fn backtrace(pre: i64, op: Op, left: &[i64], target: &i64) -> bool {
+    if left.is_empty() {
+        return pre == *target;
+    }
+
+    match op {
+        Op::Plus => {
+            if backtrace(pre + left[0], Op::Plus, &left[1..], target) {
+                return true;
+            }
+            if backtrace(pre + left[0], Op::Multi, &left[1..], target) {
+                return true;
+            }
+        }
+        Op::Multi => {
+            if backtrace(pre * left[0], Op::Plus, &left[1..], target) {
+                return true;
+            }
+            if backtrace(pre * left[0], Op::Multi, &left[1..], target) {
+                return true;
+            }
+        }
+    };
+
+    false
+}
